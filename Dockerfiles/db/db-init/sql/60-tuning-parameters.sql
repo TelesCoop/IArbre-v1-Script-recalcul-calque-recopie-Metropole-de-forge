d49b4f099e5eabf63a138cba244dd61deb86877d
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2024-07-18 16:54:30 +0200

    tuuning db

--- Dockerfiles/db/db-init/sql/60-tuning-parameters.sql
+++ Dockerfiles/db/db-init/sql/60-tuning-parameters.sql
@@ -1,7 +1,7 @@
 ALTER SYSTEM SET port TO 5432;
-
-ALTER SYSTEM SET shared_buffers TO '384MB';
-
 ALTER SYSTEM SET wal_level TO 'minimal';
-
 ALTER SYSTEM SET max_wal_senders TO 0;
+ALTER SYSTEM SET effective_cache_size TO '375MB';
+ALTER SYSTEM SET shared_buffers TO '125MB';
+ALTER SYSTEM SET work_mem TO '4MB'; -- (valeur par défaut pour pas trop descendre)
+ALTER SYSTEM SET maintenance_work_mem TO '64MB'; -- (valeur par défaut pour pas trop descendre)
